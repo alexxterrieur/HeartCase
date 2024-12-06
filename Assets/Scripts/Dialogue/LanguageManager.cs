db1966405713a526a0455b44c42e1f2f066fcb13
ElectroSpeed <150343183+ElectroSpeed@users.noreply.github.com>
2024-12-06 09:36:24 +0100

    Dialogue
    - Dialogue Script
    - Dialogues Script
    - DialogueManager Script
    - LanguageManager Script

---
+++ Assets/Scripts/Dialogue/LanguageManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class LanguageManager : MonoBehaviour
+{
+    public Languages language;
+    
+    public enum Languages
+    {
+        French,
+        English
+    }
+}
