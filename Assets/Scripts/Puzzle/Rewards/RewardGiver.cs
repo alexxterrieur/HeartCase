6d1064ebc5cb5e9f543c9914e60f20dae266ef7a
Aurelien <aure.dugue@gmail.com>
2024-12-09 10:23:21 +0100

    add reward system

--- Assets/Scripts/Puzzle/Rewards/RewardGiver.cs
+++ Assets/Scripts/Puzzle/Rewards/RewardGiver.cs
@@ -4,14 +4,19 @@ public class RewardGiver : MonoBehaviour
 {
     public void GiveReward(SO_Reward rewardSO)
     {
-        if(rewardSO == null)
+        if (rewardSO == null)
         {
             throw new System.Exception("The reward Scriptable object is null, no reward can be gived");
         }
 
         if (rewardSO.reward)
         {
+            Inventory.Instance.AddItem(rewardSO.reward);
+        }
 
+        if (rewardSO.boolId >= 0 && rewardSO.boolIndex >= 0)
+        {
+            GameState.Instance.SetBool(true, rewardSO.boolIndex, rewardSO.boolId);
         }
     }
 }
