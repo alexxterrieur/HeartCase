cf4d0a07c33b939cc7dc801843c732a185ef5261
Undergamer64 <baptiste.dufosse2005@gmail.com>
2024-12-10 10:44:06 +0100

    finished cliquable object with animations
    added events for interactions with animations

--- Assets/Scripts/Interaction/Interactions Class/AnimationInteraction.cs
+++ Assets/Scripts/Interaction/Interactions Class/AnimationInteraction.cs
@@ -1,21 +1,46 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class AnimationInteraction : Interactions
 {
     [SerializeField] private Animator animator;
-    [SerializeField] private AnimationClip animation;
+    [SerializeField] private List<UnityEvent> onAnimationFinished = new();
+    private int currentAnimation = 0;
+    private bool interactable = true;
     
     public override void Interact()
     {
-        if (animator != null)
+        if (animator != null && interactable)
         {
-            animator.Play(animation.name);
+            animator.SetTrigger("Interact");
         }
     }
 
     public override bool InteractWithItem(Item itemGived)
     {
         return false;
+    }
+
+    public void DeactivateInteraction()
+    {
+        interactable = false;
+    }
+
+    public void ActivateInteraction()
+    {
+        interactable = true;
+    }
+
+    public void AnimationFinished()
+    {
+        if (onAnimationFinished == null || onAnimationFinished.Count == 0) return;
+        onAnimationFinished[currentAnimation].Invoke();
+        
+        currentAnimation++;
+        if (currentAnimation >= onAnimationFinished.Count)
+        {
+            currentAnimation = 0;
+        }
     }
 }
