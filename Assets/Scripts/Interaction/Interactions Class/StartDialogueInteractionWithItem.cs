113e9d6f8a3128586872be3e78ded72d3c3f1b99
Aurelien <aure.dugue@gmail.com>
2024-12-17 16:57:39 +0100

    Setup dialogues in cabaret

--- Assets/Scripts/Interaction/Interactions Class/StartDialogueInteractionWithItem.cs
+++ Assets/Scripts/Interaction/Interactions Class/StartDialogueInteractionWithItem.cs
@@ -3,13 +3,41 @@ using UnityEngine;
 
 public class StartDialogueInteractionWithItem : Interactions
 {
-    [SerializeField] private Dialogue dialogueWhenDontHaveItem;
+    [SerializeField] private List<Dialogue> dialogueWhenDontHaveItem = new List<Dialogue>();
     [SerializeField] private List<Dialogue> dialogues = new List<Dialogue>();
     [SerializeField] private DialogueManager dialogueManager;
 
     public override void Interact()
     {
-        dialogueManager.StartDialogue(dialogueWhenDontHaveItem);
+        int dialogueIndex = 0;
+        foreach (Dialogue dialogue in dialogueWhenDontHaveItem)
+        {
+            int conditionsNumber = dialogue.conditions.Count;
+            if (conditionsNumber == 0)
+            {
+                dialogueIndex = dialogueWhenDontHaveItem.IndexOf(dialogue);
+                break;
+            }
+
+            int conditionsCheck = 0;
+            foreach (Condition condition in dialogue.conditions)
+            {
+                if (!GameState.Instance.GetBool(condition.boolListIndex, condition.boolId))
+                {
+                    break;
+                }
+                conditionsCheck++;
+            }
+
+            if (conditionsCheck == conditionsNumber)
+            {
+                dialogueIndex = dialogueWhenDontHaveItem.IndexOf(dialogue);
+                break;
+            }
+        }
+
+        Dialogue displayedDialogue = dialogueWhenDontHaveItem[dialogueIndex];
+        dialogueManager.StartDialogue(displayedDialogue);
     }
 
     public override bool InteractWithItem(Item itemGived)
