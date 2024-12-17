a71784f81c9b69451d108d316d6b8757f9b18b7e
Undergamer64 <baptiste.dufosse2005@gmail.com>
2024-12-17 15:33:01 +0100

    finished rework of cinematics
    added a simple intro cinematic with a prefab

---
+++ Assets/Scripts/Cinematics/Cinematic.cs
@@ -0,0 +1,155 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Cinematic : MonoBehaviour, IPointerClickHandler
+{
+    private Image uiImage;
+    
+    [SerializeField] private UnityEvent OnCinematicFinished;
+    
+    [SerializeField] private List<Image> cinematicImages = new List<Image>();
+    [SerializeField] private List<TextMeshProUGUI> cinematicTexts = new List<TextMeshProUGUI>();
+    
+    private bool cooldownActive = false;
+    
+    [SerializeField] private float maxCooldown = 2f;
+    private float cooldown;
+    private int currentIndex;
+    
+    private TextMeshProUGUI oldText = null;
+
+    private void Awake()
+    {
+        uiImage = GetComponent<Image>();
+        ResetSelf();
+    }
+
+    public void StartCinematic()
+    {
+        ResetSelf();
+        uiImage.enabled = true;
+
+        if (cinematicImages[currentIndex] is not null)
+        {
+            AddAlpha(cinematicImages[currentIndex], 1);
+        }
+
+        if (cinematicTexts[currentIndex] is not null)
+        {
+            oldText = cinematicTexts[currentIndex];
+            AddAlpha(oldText, 1);
+        }
+        currentIndex++;
+        
+        cooldown = maxCooldown;
+        cooldownActive = true;
+    }
+    
+    private void Update()
+    {
+        if (!cooldownActive) return;
+        
+        cooldown -= Time.deltaTime;
+        if (cooldown <= 0)
+        {
+            DisplayNextCinematicFrame();
+        }
+    }
+
+    private void DisplayNextCinematicFrame()
+    {
+        cooldownActive = false;
+        cooldown = maxCooldown;
+        
+        StartCoroutine(FadeInNextFrame(1, 50));
+    }
+
+    private IEnumerator FadeInNextFrame(float time, int steps)
+    {
+        if (currentIndex >= cinematicImages.Count || currentIndex >= cinematicTexts.Count)
+        {
+            EndCinematic();
+            yield break;
+        }
+        
+        TextMeshProUGUI currentText = cinematicTexts[currentIndex];
+        
+        Image currentImage = cinematicImages[currentIndex];
+        
+        float ratio = (255f / (float)steps) / 100f;
+        for (int i = 0; i < steps; i++)
+        {
+            if (currentText is not null && oldText is not null) AddAlpha(oldText, -ratio);
+            
+            yield return new WaitForSeconds(time / steps);
+        }
+        
+        yield return new WaitForSeconds(0.25f);
+        
+        for (int i = 0; i < steps; i++)
+        {
+            if (currentText is not null) AddAlpha(currentText, ratio);
+            
+            if (currentImage is not null) AddAlpha(currentImage, ratio);
+            
+            yield return new WaitForSeconds(time / steps);
+        }
+
+        if (currentText is not null) oldText = currentText;
+        
+        currentIndex++;
+        cooldown = maxCooldown;
+        cooldownActive = true;
+    }
+    
+    private void AddAlpha(Image image, float alphaToAdd)
+    {
+        Color currentColor = image.color;
+        image.color = new Color(currentColor.r, currentColor.g, currentColor.b, currentColor.a + alphaToAdd);
+    }
+    
+    private void AddAlpha(TextMeshProUGUI text, float alphaToAdd)
+    {
+        Color currentColor = text.color;
+        text.color = new Color(currentColor.r, currentColor.g, currentColor.b, currentColor.a + alphaToAdd);
+    }
+
+    private void EndCinematic()
+    {
+        cooldownActive = false;
+        ResetSelf();
+        OnCinematicFinished.Invoke();
+    }
+    
+    private void ResetSelf()
+    {
+        currentIndex = 0;
+        cooldown = maxCooldown;
+
+        foreach (Image image in cinematicImages)
+        {
+            if (image is not null)
+                image.color = new Color(image.color.r, image.color.g, image.color.b, 0);
+        }
+
+        foreach (TextMeshProUGUI text in cinematicTexts)
+        {
+            if (text is not null)
+                text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
+        }
+        
+        uiImage.enabled = false;
+    }
+    
+    //If the player clicks, skip the cooldown
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!cooldownActive) return;
+        DisplayNextCinematicFrame();
+    }
+}
