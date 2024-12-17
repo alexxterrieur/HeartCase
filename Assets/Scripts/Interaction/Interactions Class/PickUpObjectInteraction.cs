ba12ff44f645af493516e6fca004fb9a09592925
Aurelien <aure.dugue@gmail.com>
2024-12-17 22:30:09 +0100

    fix sprite bug + remove midas disaper cond

--- Assets/Scripts/Interaction/Interactions Class/PickUpObjectInteraction.cs
+++ Assets/Scripts/Interaction/Interactions Class/PickUpObjectInteraction.cs
@@ -5,11 +5,6 @@ public class PickUpObjectInteraction : Interactions
     [SerializeField] private ItemInspector itemInspector;
     [SerializeField] private Item itemGived;
 
-    private void Start()
-    {
-        GetComponent<SpriteRenderer>().sprite = itemGived.visual;
-    }
-
     public override void Interact()
     {
         print("interact");
